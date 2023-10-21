519037dd056b613ebda4e537aa830d81c1400a4f
jose <josetorresgarrido@gmail.com>
2023-10-21 09:34:43 -0500

    algorithms

--- app/src/main/java/org/josebaperu/algorithms/miscelaneous/SumOfTwoNumbs.java
+++ app/src/main/java/org/josebaperu/algorithms/miscelaneous/SumOfTwoNumbs.java
@@ -1,34 +1,24 @@
 package org.josebaperu.algorithms.miscelaneous;
 
 import java.util.Arrays;
-import java.util.List;
-
+import java.util.HashMap;
+import java.util.Map;
 public class SumOfTwoNumbs {
     public static void main(String[] args) {
-        int[]numbers = {3,3};
-        System.out.println(Arrays.toString(sumOfTwo(numbers,6)));
+        int[]numbers = {2,7,11,15};
+        System.out.println(Arrays.toString(sumOfTwo(numbers,9)));
     }
 
     static int[] sumOfTwo(int[] nums, int target){
-
+        Map<Integer, Integer> map = new HashMap<>();
         for(int idx = 0; idx < nums.length ; idx++){
-            int complement = target - nums[idx];
-            int i = lastIndexOf(nums,complement);
-            if( i > -1)
-                return new int[] {idx,i};
-
+            int current = nums[idx];
+            int complement = target - current;
+            if(map.containsKey(current))
+                return new int[] {map.get(current),idx};
+            map.put(complement, idx);
         }
-
         return new int[0];
     }
-    private static int lastIndexOf(int[] array, int key) {
-        int returnvalue = -1;
-        for (int i = array.length -1; i >=0 ; --i) {
-            if (key == array[i]) {
-                returnvalue = i;
-                break;
-            }
-        }
-        return returnvalue;
-    }
+
 }
