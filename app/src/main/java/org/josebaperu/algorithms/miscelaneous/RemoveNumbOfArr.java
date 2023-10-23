6a33265426b25cf0b37391770d73157364050173
jose <josetorresgarrido@gmail.com>
2023-10-23 14:21:01 -0500

    algorithms

--- app/src/main/java/org/josebaperu/algorithms/miscelaneous/RemoveNumbOfArr.java
+++ app/src/main/java/org/josebaperu/algorithms/miscelaneous/RemoveNumbOfArr.java
@@ -3,13 +3,14 @@ package org.josebaperu.algorithms.miscelaneous;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RemoveDupesSortedArr {
+public class RemoveNumbOfArr {
     public static void main(String[] args) {
         int[] arr= {0,0,1,1,1,2,2,3,3,4};
         //int[] arr= {1,1,2};
-        System.out.println(removeDuplicates(arr));
+        System.out.println(doRemove(arr , 1));
+        //TODO
     }
-    static int removeDuplicates(int[] nums) {
+    static int doRemove(int[] nums, int n) {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length ; i++){
             if(!map.containsKey(nums[i])){
