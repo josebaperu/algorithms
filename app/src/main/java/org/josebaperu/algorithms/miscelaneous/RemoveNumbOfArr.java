4e5d1ce60cf6fdeadd4f3485c540dacb1d0e5741
jose <josetorresgarrido@gmail.com>
2023-10-23 16:17:56 -0500

    algorithms

--- app/src/main/java/org/josebaperu/algorithms/miscelaneous/RemoveNumbOfArr.java
+++ app/src/main/java/org/josebaperu/algorithms/miscelaneous/RemoveNumbOfArr.java
@@ -1,24 +1,21 @@
 package org.josebaperu.algorithms.miscelaneous;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RemoveNumbOfArr {
     public static void main(String[] args) {
         int[] arr= {0,0,1,1,1,2,2,3,3,4};
-        //int[] arr= {1,1,2};
         System.out.println(doRemove(arr , 1));
-        //TODO
     }
     static int doRemove(int[] nums, int n) {
-        Map<Integer,Integer> map = new HashMap<>();
+        int counter = 0;
         for(int i = 0; i < nums.length ; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],i );
-                nums[map.size()-1] = nums[i];
+            int numByIdx = nums[i];
+            if(numByIdx == n){
+                counter++;
+            } else {
+                int tempIdx = i-counter;
+                nums[tempIdx] = numByIdx;
             }
         }
-
-        return map.size();
+        return nums.length - counter;
     }
 }
---
+++ app/src/main/java/org/josebaperu/algorithms/miscelaneous/SearchInsertPos.java
@@ -0,0 +1,14 @@
+package org.josebaperu.algorithms.miscelaneous;
+
+public class SearchInsertPos {
+    public static void main(String[] args) {
+
+    }
+    static int searchInsert(int[] nums, int target) {
+
+
+
+
+        return -1;
+    }
+}
