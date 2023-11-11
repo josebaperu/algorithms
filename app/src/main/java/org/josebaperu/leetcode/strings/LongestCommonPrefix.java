80bfcca3ad4ac5f59f105bc264d110f9d7f8da82
jose <josetorresgarrido@gmail.com>
2023-11-11 14:29:51 -0500

    11nov23

---
+++ app/src/main/java/org/josebaperu/leetcode/strings/LongestCommonPrefix.java
@@ -0,0 +1,12 @@
+package org.josebaperu.leetcode.strings;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+    static String longestCommonPrefix(String[] strs) {
+    return "";
+    }
+}
