ab4b38fdab3bd83c1d157197c8a9b72c0e1b5938
jose <josetorresgarrido@gmail.com>
2023-11-16 19:14:05 -0500

    LongestCommonPrefix

--- app/src/main/java/org/josebaperu/leetcode/RemoveElements.java
+++ app/src/main/java/org/josebaperu/leetcode/RemoveElements.java
@@ -1,24 +1,34 @@
 package org.josebaperu.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Stack;
 
 public class RemoveElements {
     public static void main(String[] args) {
-        int[]nums = {0,1,2,3,4,5,5,6};
-        System.out.println(removeElement(nums,5));
+        int[]nums = {0,1,1,1,4,5,5,5,1};//1
+        int[]nums1 = {0,1,2,3,4,5,6,2};//2
+        System.out.println(removeElement(nums,1));
+        //System.out.println(removeElement(nums1,2));
     }
     static int removeElement(int[] nums, int val) {
         if(nums.length == 0)return 0;
-        if(nums.length == 1)return 0;
-        Map<Integer,Integer> mapValsIdx = new HashMap<>();
-        for(int i=0; i< nums.length;i++){
-            int current = nums[i];
-            if(current == val){
+        if(nums.length == 1)return nums[0] == val?0:nums[0];
 
+        Stack<Integer> stack = new Stack<>();
+        int last = nums.length-1;
+
+        while( last >= 0){
+            if(nums[last] != val && last >=0){
+                stack.push(nums[last]);
             }
+            last--;
         }
-        return 0;
+        int maxStack = stack.size()-1 ;
+        int result = stack.size();
+        while(maxStack > -1){
+            nums[maxStack] = stack.pop();
+            maxStack--;
+        }
+
+        return result;
     }
 }
