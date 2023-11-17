04c4a922087503af562a37564678e007271caa34
jose <josetorresgarrido@gmail.com>
2023-11-16 23:59:25 -0500

    LongestCommonPrefix

--- app/src/main/java/org/josebaperu/leetcode/RemoveElements.java
+++ app/src/main/java/org/josebaperu/leetcode/RemoveElements.java
@@ -1,12 +1,14 @@
 package org.josebaperu.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class RemoveElements {
     public static void main(String[] args) {
-        int[]nums = {0,1,1,1,4,5,5,5,1};//1
+        int[]nums = {3,2,2,3};//1
         int[]nums1 = {0,1,2,3,4,5,6,2};//2
-        System.out.println(removeElement(nums,1));
+        System.out.println(removeElement(nums,3));
         //System.out.println(removeElement(nums1,2));
     }
     static int removeElement(int[] nums, int val) {
@@ -17,18 +19,17 @@ public class RemoveElements {
         int last = nums.length-1;
 
         while( last >= 0){
-            if(nums[last] != val && last >=0){
+            if(nums[last] != val){
                 stack.push(nums[last]);
             }
             last--;
         }
-        int maxStack = stack.size()-1 ;
         int result = stack.size();
-        while(maxStack > -1){
-            nums[maxStack] = stack.pop();
-            maxStack--;
+        int idx = 0;
+        while(!stack.isEmpty()){
+            nums[idx] = stack.pop();
+            idx++;
         }
-
         return result;
     }
 }
