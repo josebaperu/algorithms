e7918eba4f172876e9880ff3c4bb318a0176dc0a
jose <josetorresgarrido@gmail.com>
2023-11-24 20:39:39 -0500

    23NOV

---
+++ app/src/main/java/org/josebaperu/leetcode/SymmetricTree.java
@@ -0,0 +1,24 @@
+package org.josebaperu.leetcode;
+
+import org.josebaperu.common.TreeNode;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1, new TreeNode(2, new TreeNode(3),new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        System.out.println(isSymmetric(node));
+    }
+    static boolean isSymmetric(TreeNode node) {
+        if (null == node) return false;
+        return isMirror(node.left, node.right);
+    }
+    static boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+
+        return left.val == right.val && isMirror(left.right, right.left) && isMirror(left.left, right.right);
+    }
+}
