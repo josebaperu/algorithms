1b0b35b450cae1b422e4b5854a0eb4ff720bf015
jose <josetorresgarrido@gmail.com>
2023-11-24 20:53:10 -0500

    23NOV

---
+++ app/src/main/java/org/josebaperu/leetcode/SameTree.java
@@ -0,0 +1,20 @@
+package org.josebaperu.leetcode;
+
+import org.josebaperu.common.TreeNode;
+
+public class SameTree {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1, new TreeNode(2, new TreeNode(3),new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        TreeNode node2 = new TreeNode(1, new TreeNode(2, new TreeNode(3),new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+        System.out.println(isSameTree(node1, node2));
+    }
+    static boolean isSameTree(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        return left.val == right.val && isSameTree(left.left,right.left) && isSameTree(left.right,right.right) ;
+    }
+}
