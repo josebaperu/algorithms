e658562412409bf1b161fb9ce5d4da6b89ef8d7f
jose <josetorresgarrido@gmail.com>
2023-12-07 11:27:38 -0500

    ContainsDupe

--- app/src/main/java/org/josebaperu/leetcode/ContainsDupe.java
+++ app/src/main/java/org/josebaperu/leetcode/ContainsDupe.java
@@ -5,18 +5,20 @@ import java.util.Map;
 
 public class ContainsDupe {
     public static void main(String[] args) {
-        int[] arr = {1,1,1,3};
+        int[] arr = {1,3,4,5,5};
         System.out.println(containsDuplicate(arr));
     }
     static boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int n: nums){
-            if(!map.containsKey(n)){
-                map.put(n,n);
-            }else{
+        Map<Integer,Integer> uniqueMap = new HashMap<>();
+        for(int i = 0; i< nums.length; i++) {
+
+            int current = nums[i];
+            if (uniqueMap.containsKey(current)) {
                 return true;
             }
+            uniqueMap.put(current, i);
         }
+
         return false;
     }
 }
